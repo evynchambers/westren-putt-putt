402b948f4cbb04d2a4c1850cbc209ad0a50ee7ca
evynchambers <34458645+evynchambers@users.noreply.github.com>
2017-12-19 16:11:05 -0200

    changes

--- Dawes Putt Putt/Assets/ballcontrol1.cs
+++ Dawes Putt Putt/Assets/ballcontrol1.cs
@@ -8,22 +8,64 @@ public class ballcontrol1 : MonoBehaviour {
     //public Transform clubObj;
     public float zforce = 100;
     public Transform arrowObj;
+    private bool isShot;
+    public Rigidbody golfBall;
+    public GameObject ball;
+    private int shotsTaken;
+    public int[] pars;
+    private int parTotal;
+    private int currentHole;
+    public int[] score;
 
+    void addPar()
+    {
+        for (int i = 0; i < pars.Length; i++)
+        {
+            parTotal += pars[i];
+        }
+    }
 
+    //score[currentHole] = pars[currentHole] - shotsTaken;
 
 	// Use this for initialization
 	void Start () {
+
+        isShot = false;
+        shotsTaken = 0;
+        parTotal = 0;
+        currentHole = 0;
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetButtonDown("up"))
+        if(golfBall.velocity == Vector3.zero)
         {
-            GetComponent<Rigidbody>().AddRelativeForce(0, 0, zforce);
+            if(isShot == true)
+            {
+                isShot = false;
+                ball.transform.position = transform.position;
+                this.transform.parent = ball.transform;
+                arrowObj.transform.parent = ball.transform;
+                golfBall.transform.rotation = ball.transform.rotation;
+                arrowObj.transform.position = transform.position;
+
+            }
         }
 
+        if (Input.GetButtonDown("up") && isShot == false)
+        {
+            shotsTaken++;
+            Debug.Log(shotsTaken);
+            isShot = true;
+            ball.transform.DetachChildren();
+            //golfBall.AddRelativeForce(0, 0, zforce);
+            golfBall.AddForce(transform.forward * zforce);
+
+        }
+
+        //fix
         if (Input.GetKeyDown("space"))
         {
             GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
@@ -34,11 +76,12 @@ public class ballcontrol1 : MonoBehaviour {
 
         if (Input.GetKey("a"))
         {
-            transform.Rotate(0, -1, 0);
+            ball.transform.Rotate(0, -1, 0);
+           
         }
         if (Input.GetKey("d"))
         {
-            transform.Rotate(0, 1, 0);
+            ball.transform.Rotate(0, 1, 0);
         }
 
         if (Input.GetButton("one"))
@@ -70,9 +113,10 @@ public class ballcontrol1 : MonoBehaviour {
     {
         if(other.name == "cup")
         {
+            //score[currentHole] = pars[currentHole] - shotsTaken;
             Debug.Log("Completed");
-            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            //GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
             ;        }
     }
 }
